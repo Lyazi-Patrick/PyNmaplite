70fda53211a5fdbb60e5e08e3c7d4b7384da869b
LYAZI PATRICK <Lyazipatrick17@gmail.com>
2025-09-18 18:59:28 +0300

    add command-line interface and argument parsing

--- PyNmaplite/cli.py
+++ PyNmaplite/cli.py
@@ -1,20 +1,38 @@
 import argparse
-from .scanner import run_tcp_scan
+from .scanner import run_tcp_scan, grab_banner
+from .utils import save_to_json, save_to_csv
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="PyNmaplite - Lightweight port & service scanner")
+    parser.add_argument("--target", required=True, help="Target IP or hostname")
+    parser.add_argument("--start", type=int,default=1,help="start port (default 1)")
+    parser.add_argument("--end", type=int, default=1024,help="End Port (default 1024)")
+    parser.add_argument("--banners", action="store_true", help="Attempt to Grab service banners for open ports")
+    parser.add_argument("--json", action="store_true", help="save results to JSON")
+    parser.add_argument("--csv", action="store_true", help="Save results to CSV")
+    return parser.parse_args()
 
 def main():
-    parser = argparse.ArgumentParser(description="PyNmapLite - Python Port Scanner")
-    parser.add_argument("--target", required=True, help="Target IP or hostname")
-    parser.add_argument("--start", type=int, default=1, help="Start port(default 1)")
-    parser.add_argument("--end", type=int, default=1024, help = "End port (default 1024)")
+    args = parse_args()
+    target = args.target
+    start_port = args.start
+    end_port = args.end
 
-    args = parser.parse_args()
+    print(f"\n[+] Scanning {target} from port {start_port} to {end_port}...\n")
+    open_ports = run_tcp_scan(target, start_port, end_port)
 
-    print(f"\n[+] Scanning {args.target}  from port {args.start} to {args.end}...")
-    open_ports = run_tcp_scan(args.target, args.start, args.end)
+    if not open_ports:
+        print("[-] No open ports found.")
+        return
+    
+    results = []
+    print("[+] Open ports found:")
+    for port in open_ports:
+        banner = grab_banner(target, port) if args.banners else "Unknown"
+        print(f"-Port {port} | Banner:{banner}")
 
-    if open_ports:
-        print("\n[+] Open ports found:")
-        for port in open_ports:
-            print(f"  -Port {port}")
-    else:
-        print("\n[-] No open Ports Found.")
+    #Save if requested
+    if args.json:
+        save_to_json(results)
+    if args.csv:
+        save_to_csv(results)
